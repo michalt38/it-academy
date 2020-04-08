d8005a31b9803583913f1a8f3d3687ea769452d0
michalt38 <michalt.krakow@gmail.com>
2020-04-08 20:09:28 +0200

    pow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int pow(int x, int n)
+{
+    int result = x;
+
+    for(; n > 1; --n)
+    {
+        result *= x;
+    }
+
+    return result;
+}
+
 int main()
 {
-    int i = 0;
+    int i = 2;
     cout << "Hello world!" << endl;
 }
